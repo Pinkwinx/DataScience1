07540b9c711433e6063f4fb0d841707301fa33da
Maria Nguyen <pinkwinx999@gmail.com>
2023-12-04 00:10:16 -0700

    Update to final project data analysis page

--- Final Project/HTML/GenshinAnalysis.js
+++ Final Project/HTML/GenshinAnalysis.js
@@ -1,29 +1,103 @@
-// Get the container element to enable horizontal scrolling
 const container = document.querySelector('.horizontal');
+const imageContainer = document.querySelector('.image-container');
+const lumineIdle = document.querySelector('.Lumine-Idle');
+const lumineRun = document.querySelector('.Lumine-Run');
+
+let isScrolling;
+let isArrowKeyDown = {
+    ArrowLeft: false,
+    ArrowRight: false
+};
+
+let animationInterval;
+
+function toggleGifs() {
+    lumineIdle.style.display = isScrolling || isArrowKeyDown.ArrowLeft || isArrowKeyDown.ArrowRight ? 'none' : 'block';
+    lumineRun.style.display = isScrolling || isArrowKeyDown.ArrowLeft || isArrowKeyDown.ArrowRight ? 'block' : 'none';
+}
+
+function startAnimation() {
+    if (!animationInterval) {
+        animationInterval = setInterval(function() {
+            lumineRun.style.display = 'block';
+            lumineIdle.style.display = 'none';
+        }, 100); // Adjust this interval for animation speed
+    }
+}
+
+function stopAnimation() {
+    clearInterval(animationInterval);
+    animationInterval = null;
+    lumineRun.style.display = 'none';
+    lumineIdle.style.display = 'block';
+}
 
 function handleHorizontalScroll(e) {
-    // Detect the scroll direction for different browsers
     const delta = (e.deltaY || -e.detail || e.wheelDelta);
     const direction = delta > 0 ? 1 : -1;
-    //control scroll speed
-    const scrollSpeed = 25; // Adjust this value to change scroll speed
-    const scrollDistance = 100 / scrollSpeed;
-    // Check if the user is scrolling up or down
+    const scrollSpeed = 1;
+
     if (direction > 0) {
-        container.scrollLeft += 100; // Scroll to the right
+        container.scrollLeft += 100;
     } else {
-        container.scrollLeft -= 100; // Scroll to the left
+        container.scrollLeft -= 100;
     }
 
-    // Prevent the default vertical scrolling behavior
     e.preventDefault();
+
+    isScrolling = true;
+    toggleGifs();
+    setTimeout(function() {
+        isScrolling = false;
+        toggleGifs();
+    }, 200); // Adjust this value to suit your desired delay
 }
 
-// Add event listener to the entire document for scrolling
+function handleArrowKeys(e) {
+    if (e.keyCode === 37) {
+        container.scrollLeft -= 100;
+        e.preventDefault();
+        isArrowKeyDown.ArrowLeft = true;
+        startAnimation();
+    } else if (e.keyCode === 39) {
+        container.scrollLeft += 100;
+        e.preventDefault();
+        isArrowKeyDown.ArrowRight = true;
+        startAnimation();
+    }
+
+    isScrolling = true;
+    toggleGifs();
+    setTimeout(function() {
+        isScrolling = false;
+        toggleGifs();
+    }, 200); // Adjust this value to suit your desired delay
+}
+
+function handleKeyUp(e) {
+    if (e.keyCode === 37) {
+        isArrowKeyDown.ArrowLeft = false;
+        stopAnimation();
+    } else if (e.keyCode === 39) {
+        isArrowKeyDown.ArrowRight = false;
+        stopAnimation();
+    }
+
+    isScrolling = false;
+    toggleGifs();
+}
+
 if (document.addEventListener) {
     document.addEventListener('wheel', handleHorizontalScroll);
-    document.addEventListener('mousewheel', handleHorizontalScroll); // For older browsers
-    document.addEventListener('DOMMouseScroll', handleHorizontalScroll); // For Firefox
-} else { // For older IE versions
+    document.addEventListener('mousewheel', handleHorizontalScroll);
+    document.addEventListener('DOMMouseScroll', handleHorizontalScroll);
+    document.addEventListener('keydown', handleArrowKeys);
+    document.addEventListener('keyup', handleKeyUp);
+} else {
     document.attachEvent('onmousewheel', handleHorizontalScroll);
+    document.attachEvent('onkeydown', handleArrowKeys);
+    document.attachEvent('onkeyup', handleKeyUp);
 }
+
+lumineIdle.style.display = 'block';
+lumineRun.style.display = 'none';
